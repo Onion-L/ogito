3d48dbe5364824c1ac204f898f516a93a2b93535
XL <onionl5236@gmail.com>
2025-06-30 23:48:06 +1000

    feat: add tests and implement PartialEq for Mode and Site enums

--- src/models/mode.rs
+++ src/models/mode.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Mode {
     Git,
     Tar,
@@ -18,5 +18,30 @@ impl Mode {
             "tar" => Ok(Mode::Tar),
             _ => Err(format!("Invalid mode: {}", s)),
         }
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mode_to_str() {
+        assert_eq!(Mode::Git.to_str(), "git");
+        assert_eq!(Mode::Tar.to_str(), "tar");
+    }
+
+    #[test]
+    fn mode_from_str_valid() {
+        assert_eq!(Mode::from_str("git"), Ok(Mode::Git));
+        assert_eq!(Mode::from_str("tar"), Ok(Mode::Tar));
+    }
+
+    #[test]
+    fn mode_from_str_invalid() {
+        let err = Mode::from_str("zip");
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err(), "Invalid mode: zip");
     }
 }
--- src/models/site.rs
+++ src/models/site.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Site {
     Github,
     Gitlab,
@@ -18,5 +18,29 @@ impl Site {
             "gitlab" => Ok(Site::Gitlab),
             _ => Err(format!("Invalid site: {}", s)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_str() {
+        assert_eq!(Site::Github.to_str(), "github");
+        assert_eq!(Site::Gitlab.to_str(), "gitlab");
+    }
+
+    #[test]
+    fn test_from_str_valid() {
+        assert_eq!(Site::from_str("github"), Ok(Site::Github));
+        assert_eq!(Site::from_str("gitlab"), Ok(Site::Gitlab));
+    }
+
+    #[test]
+    fn test_from_str_invalid() {
+        let err = Site::from_str("gitee");
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err(), "Invalid site: gitee");
     }
 }
