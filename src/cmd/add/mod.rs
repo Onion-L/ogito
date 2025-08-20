5ea45681f41e2f3cee0ad9175494d00a767de75b
XL <onionl5236@gmail.com>
2025-08-20 16:35:36 +1000

    feat(add): implement template cloning and enhance output

--- src/cmd/add/mod.rs
+++ src/cmd/add/mod.rs
@@ -1,10 +1,15 @@
 mod config;
 
-use crate::file::cache::get_cache_root;
-use crate::regex::{extract_host, extract_path, is_valid_url};
+use crate::{
+    file::cache::get_cache_root,
+    mode::Mode,
+    regex::{extract_path, is_valid_url},
+};
 use clap::ArgMatches;
 use color_eyre::{eyre::eyre, Result};
 use config::{Template, TomlConfig};
+use console::style;
+use std::fs;
 
 pub async fn run(matches: &ArgMatches) -> Result<()> {
     let url = matches
@@ -21,15 +26,39 @@ pub async fn run(matches: &ArgMatches) -> Result<()> {
     };
 
     let template = build_template(matches, url)?;
+    let templates_dir = get_cache_root().join("templates");
+    let destination = templates_dir.join(&name);
+
+    if destination.exists() {
+        println!(
+            "{}",
+            style(format!(
+                "⚠️  Template directory '{}' already exists. Skipping clone.",
+                &name
+            ))
+            .yellow()
+        );
+    } else {
+        fs::create_dir_all(&destination)?;
+        let dest_string = destination
+            .to_str()
+            .ok_or_else(|| eyre!("Failed to convert destination path to string"))?
+            .to_string();
+
+        let clone_config =
+            crate::fetch::config::Config::from(&dest_string, Mode::Git, false, true, None);
+
+        crate::clone::clone(&template.url, &clone_config).await?;
+    }
+
     let config_path = get_cache_root().join("template.toml");
     let mut config = TomlConfig::load(&config_path)?;
-    config.add_template(name.clone(), template);
+    config.add_template(name.clone(), template.clone());
     config.save()?;
 
     Ok(())
 }
 
-/// Parses command-line arguments to construct the template name and data.
 fn build_template(matches: &ArgMatches, url: &String) -> Result<Template> {
     let template = Template {
         description: matches.get_one::<String>("description").cloned(),
@@ -40,10 +69,8 @@ fn build_template(matches: &ArgMatches, url: &String) -> Result<Template> {
     Ok(template)
 }
 
-/// Generates a default template name from a git URL, e.g., "github.com:user/repo".
 fn generate_default_name(url: &String) -> Result<String> {
     let (owner, repo) =
         extract_path(url).ok_or_else(|| eyre!("Could not extract path from URL"))?;
-    let host = extract_host(url).unwrap_or_else(|| "unknown".to_string());
-    Ok(format!("{}:{}/{}", host, owner, repo))
+    Ok(format!("{}/{}", owner, repo))
 }
